97e0e7bff52b93b4f09459cf7a030d6a7d5939b3
Andrey <a.u.golikov@gmail.com>
2023-04-17 14:15:51 +0300

    add chars

---
+++ characters.py
@@ -0,0 +1,91 @@
+import pygame
+
+# Создание общего класса главных персонажей
+class Character:
+    def __init__(self, x, y, width, height, color, max_health, max_energy, image_path):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.health = max_health
+        self.max_health = max_health
+        self.energy = max_energy
+        self.max_energy = max_energy
+        self.inventory = [None] * 5
+        self.velocity = 5
+        self.jump_height = 10
+        self.is_jumping = False
+        self.action_key = False
+        self.image = pygame.image.load(image_path)
+    # функция отрисокви персонажа
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
+    
+    # функция, отвечающая за передвижение персонажей
+    def move(self, dx, dy):
+        self.x += dx * self.velocity
+        self.y += dy * self.velocity
+    
+    # функция реализующая прыжок
+    def jump(self):
+        if not self.is_jumping:
+            self.is_jumping = True
+
+    def interact(self):
+        self.action_key = True
+
+    def update(self):
+        if self.is_jumping:
+            # Реализация прыжка (с учетом гравитации)
+            pass
+
+        if self.action_key:
+            # Реализация взаимодействия с объектами
+            pass
+
+        self.action_key = False
+
+    # функция получения урона
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+
+    # функция регенерация энергии
+    def regenerate_energy(self, amount):
+        self.energy += amount
+        if self.energy > self.max_energy:
+            self.energy = self.max_energy
+
+class Beavis(Character):
+    def __init__(self, x, y):
+        super().__init__(x, y, 36, 72, (100, 100, 100), 100, 100, 'graphic/beavis_image.png')
+        self.image = pygame.image.load('graphic/beavis_image.png')
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def fart(self):
+        # Реализация пукания
+        pass
+
+class Butthead(Character):
+    def __init__(self, x, y):
+        super().__init__(x, y, 36, 72, (150, 150, 150), 100, 100, 'graphic/butthead_image.png')
+        self.image = pygame.image.load('graphic/butthead_image.png')
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def burp(self):
+        # Реализация рыгания
+        pass
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import pygame
 import menu
+from room_location import Room
+
 
 # инициализация pygame 
 pygame.init()
 
-
 # расширение по умолчанию
 default_width = 1280
 default_height = 720
-
-
 
 # создание экрана 
 screen = pygame.display.set_mode((default_width, default_height))
@@ -21,18 +20,66 @@ pygame.display.set_caption('Beavis and Butthead Remaster')
 icon = pygame.image.load('graphic/dispaly_icon.png')
 pygame.display.set_icon(icon)
 
+# состояние игры
+game_state = "main_menu"
+
+# создание объекта комнаты
+room = Room(screen, 'graphic/Room(alpha).png')
+
+# кнопки управления персонажамми 
+keys = {
+    "left": False,
+    "right": False,
+    "up": False,
+    "down": False
+}
+
+
 # главный цикл игры
 while True:
     # обработчик событий 
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         if event.type == pygame.QUIT:
             # нажатие кнопки закрытия окна игры
             pygame.quit()
             raise SystemExit
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                keys["left"] = True
+            elif event.key == pygame.K_RIGHT:
+                keys["right"] = True
+            elif event.key == pygame.K_UP:
+                keys["up"] = True
+            elif event.key == pygame.K_DOWN:
+                keys["down"] = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                keys["left"] = False
+            elif event.key == pygame.K_RIGHT:
+                keys["right"] = False
+            elif event.key == pygame.K_UP:
+                keys["up"] = False
+            elif event.key == pygame.K_DOWN:
+                keys["down"] = False
         
-
-    # отбражение главного меню
-    menu.display_main_menu(screen, default_width, default_height)
+    if game_state == "main_menu":
+        # отображение главного меню
+        next_state = menu.display_main_menu(screen, default_width, default_height, events)
+        if next_state == "start_game":
+            game_state = "gameplay"
+    elif game_state == "gameplay":
+        dx, dy = 0, 0
+        if keys["left"]:
+            dx = -1
+        if keys["right"]:
+            dx = 1
+        if keys["up"]:
+            dy = -1
+        if keys["down"]:
+            dy = 1
+        room.beavis.move(dx, dy)
+        room.display()
 
     # обновление экрана
     pygame.display.update()
